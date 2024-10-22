ddce89d92657702614035600e1c1b055739a73a1
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-10-22 20:42:31 +0530

    Setup Brush size

--- app/src/main/java/com/example/drawingapp/DrawingView.kt
+++ app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -8,6 +8,7 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -35,7 +36,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+     //   mBrushSize = 20.toFloat()
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -91,6 +92,16 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
 
         invalidate()
         return true
+    }
+
+    fun setSizeForBrush(newSize: Float) {
+        mBrushSize = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            newSize,
+            resources.displayMetrics
+        )
+        mDrawPaint!!.strokeWidth = mBrushSize
+
     }
 
 
--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -3,14 +3,17 @@ package com.example.drawingapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var drawingView: DrawingView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        drawingView=findViewById(R.id.drawing_view)
+        drawingView?.setSizeForBrush(20.toFloat())
 
     }
 }
